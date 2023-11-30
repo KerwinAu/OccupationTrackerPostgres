5b1f4dabc187fbb6dde9ff2888cb054607a9c1fb
KerwinAu Au <aukerwin@yahoo.de>
2023-11-30 04:18:03 +0000

    opentelemetry

--- webshop/backend/src/main/java/com/example/WebshopApplication.java
+++ webshop/backend/src/main/java/com/example/WebshopApplication.java
@@ -4,12 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.exporter.jaeger.JaegerGrpcSpanExporter;
+
 @SpringBootApplication
 @EnableWebMvc
 public class WebshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        JaegerGrpcSpanExporter jaegerExporter = JaegerGrpcSpanExporter.builder()
+            .setEndpoint("http://jaeger:14250") // replace with your Jaeger endpoint
+            .build();
 
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+            .addSpanProcessor(SimpleSpanProcessor.create(jaegerExporter))
+            .build();
+
+        OpenTelemetrySdk.builder()
+            .setTracerProvider(tracerProvider)
+            .buildAndRegisterGlobal();
+
+        SpringApplication.run(WebshopApplication.class, args);
+    }
 }
--- webshop/backend/src/main/java/com/example/controller/UserController.java
+++ webshop/backend/src/main/java/com/example/controller/UserController.java
@@ -15,6 +15,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.example.utility.ResponseUtility;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @CrossOrigin(origins = "*")
@@ -32,15 +41,27 @@ public class UserController {
     // URL: http://localhost:8080/UserEntity
     @PostMapping
     public boolean createUser(@RequestBody UserEntity userEntity) {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("createUser").startSpan();
+    try (Scope scope = span.makeCurrent()) {
 
         return userService.create(userEntity);
-    }
+    }finally{
+        span.end();
+    }}
 
     @GetMapping("/")
-    public List<UserEntity> getAll() {
-        
+public List<UserEntity> getAll() {
+
+    Span span = GlobalOpenTelemetry.getTracer("Occupation").spanBuilder("getAll").startSpan();
+    try (Scope scope = span.makeCurrent()) {
+
         return userService.getAll();
+
+    } finally {
+        span.end();
     }
+}
+    
 
 
     //@GetMapping("/")
@@ -53,73 +74,114 @@ public class UserController {
     // URL: http://localhost:8080/UserEntity/id?id=1
     @GetMapping("/id")
     public UserEntity getById(@RequestParam(value = "id", defaultValue = "1") long id) {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("getCalcTable").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         return userService.getById(id);
-    }
+    }finally{
+        span.end();
+    }}
 
     //updates entity with id for exsample @maxCheckinsAllowed 
     @PatchMapping("/id")
     public boolean update(@RequestParam(value = "id") long id, @RequestBody UserEntity userEntity) {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("getById").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         userEntity.setId(id); // Set the id from the URL to the userEntity object
         return userService.update(id,userEntity);
-    }
+    }finally{
+        span.end();
+    }}
     
 
     // URL: http://localhost:8080/UserEntity/delete?id=1
     @DeleteMapping("/{id}")
     public boolean delete(@RequestParam long id) {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("deleteById").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         return userService.delete(id);
-    }
+    }finally{
+        span.end();
+    }}
 
     @DeleteMapping("/deleteAll")
     public boolean deleteAll() {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("deleteAll").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         return userService.deleteAll();
-    }
+    }finally{
+        span.end();
+    }}
 
     @GetMapping("/fetchData")
     public ResponseEntity<String> fetchAndSaveData() {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("fetchData").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         dataFetchingTask.fetchAndSaveData(); // Trigger the task
         return new ResponseEntity<>("Data fetching task initiated", HttpStatus.OK);
-    }
+    }finally{
+        span.end();
+    }}
 
     // URL: http://localhost:8080/UserEntity/getOccupiedRecords?selectedMonth=November
     @GetMapping("/getOccupiedRecords")
     public List<UserEntity> getOccupiedRecords(@RequestParam("selectedMonth") String selectedMonth) {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("getOccupiedReccords").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         return userService.getOccupiedRecords(selectedMonth);
-    }
+    }finally{
+        span.end();
+    }}
 
     // URL: http://localhost:8080/UserEntity/getCalcTable?selectedDay=Monday&selectedTimeOfDay=Morning
     @GetMapping("/getCalcTable")
     public List<CalcTableEntity> getCalcTable(@RequestParam(required = false) String selectedDay,
             @RequestParam(required = false) String selectedTimeOfDay) {
+                
+                    Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("getCalcTable").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         return userService.getCalcTable(selectedDay, selectedTimeOfDay);
+            } finally {
+        span.end();
+    }
+
     }
 
     // URL: http://localhost:8080/UserEntity/getLowestOccupation
     @GetMapping("/getLowestOccupation")
     public List<UserEntity> getLowestOccupation() {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("getLowestOccupation").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         return userService.getLowestOccupation();
-    }
+    }finally{span.end();}}
 
     // URL: http://localhost:8080/UserEntity/getHighestOccupation
     @GetMapping("/getHighestOccupation")
     public List<UserEntity> getHighestOccupation() {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("getHighestOccupation").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         return userService.getHighestOccupation();
-    }
+    }finally{ span.end();}}
 
     // URL: http://localhost:8080/UserEntity/getWeekdaysView
     @GetMapping("/getWeekdaysView")
     public List<UserEntity> getWeekdaysView() {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("getWeekdaysView").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         return userService.getWeekdaysView();
-    }
+    }finally{span.end();}}
 
     // URL: http://localhost:8080/UserEntity/getWeekdaysView
     @GetMapping("/getWeekenddaysView")
     public List<UserEntity> getWeekenddaysView() {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("getWeekenddaysView").startSpan();
+    try (Scope scope = span.makeCurrent()) {
         return userService.getWeekenddaysView();
-    }
+    }finally{span.end();}}
     // Url: http://localhost:8080/UserEntity/getCurrentStatus
     @GetMapping("/getCurrentStatus")
     public Map<?,?> getCurrentStatus() {
+        Span span = GlobalOpenTelemetry.getTracer("week").spanBuilder("getCurrentStatus").startSpan();
+    try (Scope scope = span.makeCurrent()) {
             String externalData = externalApiService.fetchDataFromExternalApi();
             System.out.println(externalData);
             Map<?,?> latest = null;
@@ -136,7 +198,8 @@ public class UserController {
                          latest = Map.of("maxCheckinsAllowed", maxCheckinsAllowed, "countCheckedInCustomer", countCheckedInCustomer);
                     }
                  return latest;
-                }
+                }finally{span.end();
+                }}
                     
 }
 
